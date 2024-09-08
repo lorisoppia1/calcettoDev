816963424c580aed6b32d5db19cd5d4ec2c50b3b
Loris Oppia <loris.oppia@devinterface.com>
2024-09-08 18:46:57 +0200

    Add staticfiles

--- calcettoDev/calcettoDev/settings.py
+++ calcettoDev/calcettoDev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-nlnly-(hr!judxinpch$^ocguv8wlfp(fd+#@@q&m6g$10&(f+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mazino.pythonanywhere.com']
+ALLOWED_HOSTS = ['mazino.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
