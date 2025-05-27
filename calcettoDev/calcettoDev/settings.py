3e206662e34745b254c0bb80c2122072e560ef07
Loris Oppia <loris.oppia@devinterface.com>
2025-05-27 15:31:02 +0200

    fix settings static

--- calcettoDev/calcettoDev/settings.py
+++ calcettoDev/calcettoDev/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/calcetto/static/'
+STATIC_ROOT = BASE_DIR / "calcetto" / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "calcetto" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
